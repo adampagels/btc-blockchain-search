10c6c9c768c411c7c137869862538231e0bd07f3
Adam Pagels <pagels@hotmail.ca>
2022-11-19 16:21:10 +0100

    Create card components

---
+++ src/components/AddressCard/View/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const AddressCard = () => {
+  return (
+    <View>
+      <Text>AddressCard</Text>
+    </View>
+  );
+};
+export default AddressCard;
---
+++ src/components/TransactionCard/View/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const TransactionCard = () => {
+  return (
+    <View>
+      <Text>TransactionCard</Text>
+    </View>
+  );
+};
+export default TransactionCard;
