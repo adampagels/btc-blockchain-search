ef4b0b8da90f7042dd5779d4ea0a95129ca5006e
Adam Pagels <pagels@hotmail.ca>
2022-11-19 12:09:07 +0100

    Create SearchInput component

---
+++ components/SearchInput/View/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const SearchInput = () => {
+  return (
+    <View>
+      <Text>SearchInput</Text>
+    </View>
+  );
+};
+
+export default SearchInput;
