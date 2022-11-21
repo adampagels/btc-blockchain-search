33c1d17ab1cccf1e608da20cc8097324bab0ce79
Adam Pagels <pagels@hotmail.ca>
2022-11-21 11:45:59 +0100

    Create TopSearches Component

---
+++ src/components/TopSearches/View/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const TopSearches = () => {
+  return (
+    <View testID="currency-select">
+      <Text>TOPSEARCHES</Text>
+    </View>
+  );
+};
+export default TopSearches;
