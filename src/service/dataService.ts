ce20a26a0ff4dbfe3a1170d21c50e7eeae0bb0f8
Adam Pagels <pagels@hotmail.ca>
2022-11-21 19:32:55 +0100

    Switch from axios to fetch

--- src/service/dataService.ts
+++ src/service/dataService.ts
@@ -1,13 +1,17 @@
-import axios from "axios";
-
 const API_URL = "https://chain.api.btc.com/v3";
 
 export const getAddressByHash = async (hashAddress: string) => {
   // NOTE: For testing purposes: 15urYnyeJe3gwbGJ74wcX89Tz7ZtsFDVew
-  const addressUrl = `${API_URL}/address/${hashAddress}`;
   try {
-    const fetchedAddress = await axios.get(addressUrl);
-    return fetchedAddress.data.data;
+    const fetchedAddress = await fetch(`${API_URL}/address/${hashAddress}`);
+    const addressResponse = await fetchedAddress.json();
+    if (
+      typeof addressResponse.data === "string" &&
+      addressResponse.data.includes("invalid")
+    ) {
+      return [];
+    }
+    return addressResponse.data;
   } catch (error: any) {
     return { error: error.response.data };
   }
@@ -15,10 +19,18 @@ export const getAddressByHash = async (hashAddress: string) => {
 
 export const getTransactionByHash = async (hashTransaction: string) => {
   // NOTE: For testing purposes: 0eab89a271380b09987bcee5258fca91f28df4dadcedf892658b9bc261050d96
-  const transactionUrl = `${API_URL}/tx/${hashTransaction}?verbose=3`;
   try {
-    const fetchedTransaction = await axios.get(transactionUrl);
-    return fetchedTransaction.data.data;
+    const fetchedTransaction = await fetch(
+      `${API_URL}/tx/${hashTransaction}?verbose=3`
+    );
+    const transactionResponse = await fetchedTransaction.json();
+    if (
+      typeof transactionResponse.data === "string" &&
+      transactionResponse.data.includes("invalid")
+    ) {
+      return [];
+    }
+    return transactionResponse.data;
   } catch (error: any) {
     return { error: error.response.data };
   }
